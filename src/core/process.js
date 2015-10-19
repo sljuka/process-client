57658574aa96d4cb26a708cf5cad3b59b10ef1d9
David Sljukic <dsljukic@blueberryapps.com>
2015-10-19 23:55:43 +0200

    Update processes to use names for keys instead of ids

--- src/core/process.js
+++ src/core/process.js
@@ -1,13 +1,13 @@
-import {List} from 'immutable'
+import {List, Map, fromJS} from 'immutable'
 
 export const MAX_OPENED_PROCESSES = 6
 
-export function openProcess(processState, id) {
+export function openProcess(processState, name) {
   return processState.update('opened', List(), processes => {
-    const indexOfId = processes.indexOf(id)
+    const indexOfId = processes.indexOf(name)
     if (indexOfId !== -1)
       processes = processes.remove(indexOfId)
-    processes = processes.unshift(id)
+    processes = processes.unshift(name)
     if (processes.size > MAX_OPENED_PROCESSES)
       processes = processes.splice(MAX_OPENED_PROCESSES, processes.size - MAX_OPENED_PROCESSES)
 
@@ -15,16 +15,20 @@ export function openProcess(processState, id) {
   })
 }
 
-export function closeProcess(processState, id) {
-  return processState.update('opened', List(), processes => {
-    const indexOfId = processes.indexOf(id)
+export function closeProcess(processState, name) {
+  const openedList = processState.update('opened', List(), opened => {
+    const indexOfId = opened.indexOf(name)
     if (indexOfId !== -1)
-      processes = processes.remove(indexOfId)
+      opened = opened.remove(indexOfId)
 
-    return processes
+    return opened
   })
+  console.log(openedList.toJS())
+  return openedList.deleteIn(['processes', 'p1', 'opened'])
 }
 
 export function fetchProcessesSuccess(processState, fetched) {
-  return processState.set('processes', fetched)
+  return processState.update('processes', Map(fetched), processes => {
+    return fromJS(processes).mergeDeep(fromJS(fetched))
+  })
 }
--- test/core/process_spec.js
+++ test/core/process_spec.js
@@ -10,54 +10,54 @@ describe('process application logic', () => {
 
     it('adds the process to the state', () => {
       const processState = Map()
-      const entry = 1
+      const entry = 'p1'
       const nextState = openProcess(processState, entry)
       expect(nextState).to.equal(Map({
-        opened: List.of(1)
+        opened: List.of('p1')
       }))
     })
 
     it('adds the process to the begining of list', () => {
       const processState = Map({
-        opened: List.of(2, 3, 4)
+        opened: List.of('p2', 'p3', 'p4')
       })
-      const entry = 1
+      const entry = 'p1'
       const nextState = openProcess(processState, entry)
       expect(nextState).to.equal(Map({
-        opened: List.of(1, 2, 3, 4)
+        opened: List.of('p1', 'p2', 'p3', 'p4')
       }))
     })
 
     it('moves the process to begining of list if it is already opened', () => {
       const processState = Map({
-        opened: List.of(1, 2, 3, 4)
+        opened: List.of('p1', 'p2', 'p3', 'p4')
       })
-      const entry = 4
+      const entry = 'p4'
       const nextState = openProcess(processState, entry)
       expect(nextState).to.equal(Map({
-        opened: List.of(4, 1, 2, 3)
+        opened: List.of('p4', 'p1', 'p2', 'p3')
       }))
     })
 
     it('doesn\'t change state if opening process is already opened and on first location', () => {
       const processState = Map({
-        opened: List.of(1, 2, 3, 4)
+        opened: List.of('p1', 'p2', 'p3', 'p4')
       })
-      const entry = 1
+      const entry = 'p1'
       const nextState = openProcess(processState, entry)
       expect(nextState).to.equal(Map({
-        opened: List.of(1, 2, 3, 4)
+        opened: List.of('p1', 'p2', 'p3', 'p4')
       }))
     })
 
     it(`opens ${MAX_OPENED_PROCESSES} processes max, all excess processes are thrown out of the list`, () => {
       const processState = Map({
-        opened: List.of(1, 2, 3, 4, 5, 6)
+        opened: List.of('p1', 'p2', 'p3', 'p4', 'p5', 'p6')
       })
-      const entry = 7
+      const entry = 'p7'
       const nextState = openProcess(processState, entry)
       expect(nextState).to.equal(Map({
-        opened: List.of(7, 1, 2, 3, 4, 5)
+        opened: List.of('p7', 'p1', 'p2', 'p3', 'p4', 'p5')
       }))
     })
 
@@ -67,30 +67,52 @@ describe('process application logic', () => {
 
     it('removes the process from the opened processes list', () => {
       const processState = Map({
-        opened: List.of(1, 2, 3, 4)
+        opened: List.of('p1', 'p2', 'p3', 'p4')
       })
-      const entry = 1
+      const entry = 'p1'
       const nextState = closeProcess(processState, entry)
       expect(nextState).to.equal(Map({
-        opened: List.of(2, 3, 4)
+        opened: List.of('p2', 'p3', 'p4')
       }))
     })
 
     it('doesn\'t change state if closing process isn\'t opened', () => {
       const processState = Map({
-        opened: List.of(1, 2, 3, 4)
+        opened: List.of('p1', 'p2', 'p3', 'p4')
       })
-      const entry = 5
+      const entry = 'p5'
       const nextState = closeProcess(processState, entry)
       expect(nextState).to.equal(Map({
-        opened: List.of(1, 2, 3, 4)
+        opened: List.of('p1', 'p2', 'p3', 'p4')
+      }))
+    })
+
+    it('removes the opened key from the closed process', () => {
+      const processState = Map({
+        opened:    List.of('p1', 'p2', 'p3', 'p4'),
+        processes: {
+          p1: {
+            id:     1,
+            opened: 'instance_1'
+          }
+        }
+      })
+      const entry = 'p1'
+      const nextState = closeProcess(processState, entry)
+      expect(nextState).to.equal(Map({
+        opened:    List.of('p2', 'p3', 'p4'),
+        processes: {
+          p1: {
+            id: 1
+          }
+        }
       }))
     })
   })
 
   describe('fetchProcessesSuccess function', () => {
 
-    it('merges the fetched processes to the existing process info pool', () => {
+    it('saves the fetched processes into state', () => {
       const processState = Map()
       const entry = {
         process_1: {
@@ -112,6 +134,53 @@ describe('process application logic', () => {
           process_2: {
             id:   2,
             name: 'process_2'
+          }
+        }
+      }
+      expect(nextState.toJS()).to.deep.equal(expectedValue)
+    })
+
+    it('merges the fetched process state with the existing process state', () => {
+      const processState = Map({
+        processes: {
+          process_1: {
+            id:        1,
+            name:      'process_1',
+            instances: ['instance_1', 'instance_2'],
+            opened:    'instance_2'
+          },
+          process_2: {
+            id:        2,
+            name:      'process_2',
+            instances: ['instance_1', 'instance_2']
+          }
+        }
+      })
+      const entry = {
+        process_1: {
+          id:        1,
+          name:      'process_1',
+          instances: ['instance_2', 'instance_3']
+        },
+        process_2: {
+          id:        2,
+          name:      'process_2',
+          instances: ['instance_1', 'instance_3']
+        }
+      }
+      const nextState = fetchProcessesSuccess(processState, entry)
+      const expectedValue = {
+        processes: {
+          process_1: {
+            id:        1,
+            name:      'process_1',
+            instances: ['instance_2', 'instance_3'],
+            opened:    'instance_2'
+          },
+          process_2: {
+            id:        2,
+            name:      'process_2',
+            instances: ['instance_1', 'instance_3']
           }
         }
       }
